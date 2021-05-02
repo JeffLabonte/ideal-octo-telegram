54ca1656b5c2d569a079c87579c33ea9bdb1c4a8
Jeff Labonte <grimsleepless@protonmail.com>
2021-05-02 02:16:44 -0400

    Update mixin to use helper

--- src/commons/models/fields.py
+++ src/commons/models/fields.py
@@ -1,26 +1,25 @@
-from cryptography.fernet import Fernet
 from django.db.models import fields
 
-from src.settings import settings
+from commons.helpers.cryptography_helper import CryptographyHelper
 
 
 class EncryptedFieldMixin:
     def __init__(self, *args, **kwargs):
-        self._fernet = Fernet(key=settings.SECRET_KEY)
+        self._cryptography_helper = CryptographyHelper(filename=kwargs.pop('key_name'))
         super().__init__(*args, **kwargs)
 
     def get_prep_value(self, value):
         """
         Decrypting value from Database
         """
-        value = self._fernet.decrypt(value)
+        value = self._cryptography_helper.decrypt(value)
         return super().get_prep_value(value)
 
     def get_db_prep_save(self, value, connection):
         """
         Encrypting value just before entering the database
         """
-        value = self._fernet.encrypt(data=value.encode("utf-8"))
+        value = self._cryptography_helper.encrypt(data=value)
         return super().get_db_prep_save(value, connection)
 
 
