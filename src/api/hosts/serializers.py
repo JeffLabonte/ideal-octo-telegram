5e48776e73b1f10da908a11b684b5762a77e4bb5
Jeff Labonte <grimsleepless@protonmail.com>
2021-06-06 20:47:05 -0400

    Add Validator to ip_address in serializer

--- src/api/hosts/serializers.py
+++ src/api/hosts/serializers.py
@@ -1,17 +1,20 @@
 from hosts.models import Host
+from api.hosts.validators import IPAddressValidator
 
 from rest_framework import serializers
 
 
 class HostGetSerializer(serializers.ModelSerializer):
-  ip_address = serializers.CharField()
-
-
-  class Meta:
-    model = Host
-    fields = (
-        "id",
-        "ip_address",
-        "host_credentials",
+    ip_address = serializers.CharField(
+       validators=(
+           IPAddressValidator(),
+       ),
     )
 
+    class Meta:
+        model = Host
+        fields = (
+            "id",
+            "ip_address",
+            "host_credentials",
+        )
