b31255d16c6e6994b8e39fbe07dc4028fa07347e
Jeff Labonte <grimsleepless@protonmail.com>
2021-09-19 21:03:30 -0400

    Update model to have devices

--- src/mushroom_sensors/models.py
+++ src/mushroom_sensors/models.py
@@ -1,9 +1,31 @@
 import uuid
+
 from django.db import models
 
 
-class MushroomSensors(models.Model):
-    id = models.UUIDField(default=uuid.uuid4)
-    json_data = models.JSONField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+class Device(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+    device_name = models.CharField(
+        max_length=60,
+        unique=True,
+    )
 
+
+class MushroomSensors(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+    json_data = models.JSONField()
+    device = models.ForeignKey(
+        to=Device,
+        default=None,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+    timestamp = models.DateTimeField(
+        auto_now_add=True,
+    )
