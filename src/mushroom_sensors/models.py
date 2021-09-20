d409283c11fd41249d62e5fe323733d0d839c571
Jeff Labonte <grimsleepless@protonmail.com>
2021-09-19 20:33:32 -0400

    Add app and remove not needed files

--- src/mushroom_sensors/models.py
+++ src/mushroom_sensors/models.py
@@ -1,3 +1,9 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class MushroomSensors(models.Model):
+    id = models.UUIDField(default=uuid.uuid4)
+    json_data = models.JSONField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
--- src/settings/settings.py
+++ src/settings/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "hosts",
+    "mushroom_sensors",
 ]
 
 MIDDLEWARE = [
