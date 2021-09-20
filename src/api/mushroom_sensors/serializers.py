0e573a7570178ef51224c9de6dd775d8e49ecc27
Jeff Labonte <grimsleepless@protonmail.com>
2021-09-19 21:07:42 -0400

    Create serialier to add Device

--- src/api/mushroom_sensors/serializers.py
+++ src/api/mushroom_sensors/serializers.py
@@ -3,11 +3,14 @@ from rest_framework import serializers
 from mushroom_sensors.models import MushroomSensors
 
 
+class DeviceWriteSerializer(serializers.ModelSerializer):
+    pass
+
+
 class MushroomSensorsWriteSerializer(serializers.ModelSerializer):
     sensor_data = serializers.JSONField(required=True)
+    device_name = serializers.CharField(required=True)
 
     class Meta:
         model = MushroomSensors
-        fields = [
-
-        ]
+        fields = []
