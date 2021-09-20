d70fefe72424340b2ae4041976d14a38017e97d5
Jeff Labonte <grimsleepless@protonmail.com>
2021-09-19 21:50:11 -0400

    Add admin to the urls

--- src/api/urls.py
+++ src/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from django.urls.conf import include
 
 urlpatterns = [
-    path("admin", admin.site.url),
+    path("admin", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
 ]
