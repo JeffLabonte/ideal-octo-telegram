bafac0cc5d5598633aaaa356c96da552e223ae35
Jeff Labonte <grimsleepless@protonmail.com>
2021-09-26 21:51:27 -0400

    Add devices to the url

--- src/api/sensors/urls.py
+++ src/api/sensors/urls.py
@@ -1,8 +1,10 @@
 from rest_framework.routers import SimpleRouter
 
+from api.sensors.views.devices_views import DeviceViewSet
 from api.sensors.views.sensors_views import SensorsViewSet
 
 router = SimpleRouter()
 router.register("sensors", SensorsViewSet, basename="sensors")
+router.register("devices", DeviceViewSet, basenme="devices")
 
 urlpatterns = router.urls
