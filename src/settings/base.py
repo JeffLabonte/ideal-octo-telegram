98c39f26506b0f77b289b9f4fa611119018dc2cf
Jeff Labonte <grimsleepless@protonmail.com>
2021-10-05 19:11:07 -0400

    Add settings to prep for deploy

--- src/settings/base.py
+++ src/settings/base.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+from django.core.management import utils
 
-from settings.dev import settings
+from pathlib import Path
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = settings.get("secret_key")
+SECRET_KEY = environ.get("SECRET_KEY", utils.get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
