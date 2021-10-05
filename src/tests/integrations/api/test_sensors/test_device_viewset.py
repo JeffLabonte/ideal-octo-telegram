e49ba1d3625588df4d34c7f6c91eda9c4cc5956d
Jeff Labonte <grimsleepless@protonmail.com>
2021-10-04 20:36:53 -0400

    Fix test, remove deadcode

--- src/tests/integrations/api/test_sensors/test_device_viewset.py
+++ src/tests/integrations/api/test_sensors/test_device_viewset.py
@@ -1,11 +1,12 @@
 import pytest
 from django.contrib.auth.models import User
-from pytest_drf import Returns200, Returns201, ViewSetTest
+from pytest_drf import Returns201, ViewSetTest
 from pytest_drf.authentication import AsUser
 from pytest_drf.util.urls import url_for
 from pytest_drf.views import UsesListEndpoint, UsesPostMethod
 from pytest_lambda import lambda_fixture
 from pytest_lambda.fixtures import static_fixture
+
 
 user = lambda_fixture(
     lambda: User.objects.create(
@@ -33,6 +34,7 @@ class TestDeviceViewSet(
         )
     )
 
+    @pytest.mark.django_db
     class TestCreate(
         UsesPostMethod,
         UsesListEndpoint,
