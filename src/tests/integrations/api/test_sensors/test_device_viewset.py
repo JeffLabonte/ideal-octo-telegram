4ca0275e954d859a8048c12ed6318bf234ccb827
Jeff Labonte <grimsleepless@protonmail.com>
2021-10-04 21:52:25 -0400

    Complete the test

--- src/tests/integrations/api/test_sensors/test_device_viewset.py
+++ src/tests/integrations/api/test_sensors/test_device_viewset.py
@@ -2,10 +2,20 @@ import pytest
 from django.contrib.auth.models import User
 from pytest_drf import Returns201, ViewSetTest
 from pytest_drf.authentication import AsUser
+from pytest_drf.status import Returns200
 from pytest_drf.util.urls import url_for
-from pytest_drf.views import UsesListEndpoint, UsesPostMethod
+from pytest_drf.views import UsesDetailEndpoint, UsesListEndpoint, UsesPatchMethod, UsesPostMethod
 from pytest_lambda import lambda_fixture
 from pytest_lambda.fixtures import static_fixture
+
+from sensors.models import Device, IpAddress
+
+
+@pytest.fixture
+def device():
+    device = Device(device_name="Device Test")
+    IpAddress(ip_address="127.0.0.1", device=device).save()
+    yield device
 
 
 user = lambda_fixture(
@@ -34,7 +44,6 @@ class TestDeviceViewSet(
         )
     )
 
-    @pytest.mark.django_db
     class TestCreate(
         UsesPostMethod,
         UsesListEndpoint,
