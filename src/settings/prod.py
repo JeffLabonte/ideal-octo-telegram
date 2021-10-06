da37afbe4975e285420bf903a62d3e186d8eab66
Jeff Labonte <grimsleepless@protonmail.com>
2021-10-05 23:42:05 -0400

    Setup the prod configurations

--- src/settings/prod.py
+++ src/settings/prod.py
@@ -1,1 +1,13 @@
+import os
 from settings.base import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ["DATABASE_NAME"],
+        "USER": os.environ["DATABASE_USER"],
+        "PASSWORD": os.environ["DATABASE_PASSWORD"],
+        "HOST": os.environ.get("DATABASE_HOST", "database"),
+        "PORT": 5432,
+    }
+}
