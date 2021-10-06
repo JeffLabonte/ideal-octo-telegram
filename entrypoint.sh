81efae43c365a3e2f589c9a9c86dfdb5b108df54
Jeff Labonte <grimsleepless@protonmail.com>
2021-10-06 17:48:55 -0400

    First iteration of entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Waiting 30 seconds - Give time to PGSQL to build up"
-sleep 30
-cd /opt/code && ./manage.py migrate --settings=settings.prod 
+echo "Waiting 15 seconds - Give time to PGSQL to build up"
+sleep 15
+cd /opt/code && ./manage.py makemigrations --settings=settings.prod && ./manage.py migrate --settings=settings.prod && gunicorn settings.wsgi
