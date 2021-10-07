97df5deac7bce0b5f35a6ca7329c2a0b64bdbbf1
Jeff Labonte <grimsleepless@protonmail.com>
2021-10-06 21:55:00 -0400

    Make gunicorn work locally in container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Waiting 5 seconds - Give time to PGSQL to build up"
 sleep 5
-cd /opt/code && ./manage.py makemigrations --settings=settings.prod && ./manage.py migrate --settings=settings.prod && gunicorn -w 3 -b 0.0.0.0 settings.wsgi
+cd /opt/code && ./manage.py makemigrations --settings=settings.prod && ./manage.py migrate --settings=settings.prod && gunicorn -w 3 -b 0.0.0.0:8000 settings.wsgi
