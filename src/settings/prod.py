0f92c05d1431521a0dad427c52a361005469d606
Jeff Labonte <grimsleepless@protonmail.com>
2021-10-06 21:55:12 -0400

    Add allowed_host

--- src/settings/prod.py
+++ src/settings/prod.py
@@ -1,6 +1,15 @@
 import os
 from settings.base import *
 
+
+ALLOWED_HOSTS = [
+    "load_balancer",
+    "localhost",
+    "192.168.68.129",
+    "192.168.68.117",
+    "home-server",
+]
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
