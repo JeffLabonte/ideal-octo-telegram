efedc5ec4073e44486eb1b92a2da7ed7fdf77f3e
Jeff Labonte <grimsleepless@protonmail.com>
2021-10-13 22:22:31 -0400

    Create the model Gateway to replace Device

--- src/gateway/models.py
+++ src/gateway/models.py
@@ -1,3 +1,10 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from common.models.mixins.timestamps import TimeStampMixin
+
+
+class Gateway(TimeStampMixin, models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=50, unique=True, required=True)
