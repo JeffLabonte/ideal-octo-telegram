900fdff448b3c8ff76fd42ae804c791e55f4e785
Jeff Labonte <grimsleepless@protonmail.com>
2021-10-13 22:42:32 -0400

    Update migrations

--- src/gateway/models.py
+++ src/gateway/models.py
@@ -7,4 +7,4 @@ from common.models.mixins.timestamps import TimeStampMixin
 
 class Gateway(TimeStampMixin, models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    name = models.CharField(max_length=50, unique=True, required=True)
+    name = models.CharField(max_length=50, unique=True, blank=False, null=False)
