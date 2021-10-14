9adb7a416744064aab820488d1e8708506642571
Jeff Labonte <grimsleepless@protonmail.com>
2021-10-13 22:42:22 -0400

    Remove device from the rest of the app

--- src/api/sensors/urls.py
+++ src/api/sensors/urls.py
@@ -1,10 +1,8 @@
 from rest_framework.routers import SimpleRouter
 
-from api.sensors.views.devices_views import DeviceViewSet
 from api.sensors.views.sensors_views import SensorsViewSet
 
 router = SimpleRouter()
 router.register("sensors", SensorsViewSet, basename="sensors")
-router.register("devices", DeviceViewSet, basename="devices")
 
 urlpatterns = router.urls
