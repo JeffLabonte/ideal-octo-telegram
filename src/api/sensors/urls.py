5075a6690d0c76a76184849a30a4deacadf60edb
Jeff Labonte <grimsleepless@protonmail.com>
2021-10-16 14:46:36 -0400

    Change urls to use gateway

--- src/api/sensors/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import SimpleRouter
-
-from api.sensors.views.sensors_views import SensorsViewSet
-
-router = SimpleRouter()
-router.register("sensors", SensorsViewSet, basename="sensors")
-
-urlpatterns = router.urls
--- src/api/urls.py
+++ src/api/urls.py
@@ -4,7 +4,7 @@ from django.urls.conf import include
 
 urlpatterns = [
     path("admin", admin.site.urls),
-    path("api/", include("api.sensors.urls")),
+    path("api/", include("api.gateway.urls")),
     path("api/auth/", include("dj_rest_auth.urls")),
     path("api/auth/registration", include("dj_rest_auth.registration.urls")),
 ]
