258433a6e2f88132fc3de3af45eeb6c5fb103fdb
Jeff Labonte <grimsleepless@protonmail.com>
2021-10-17 11:44:02 -0400

    Add mac_address to model

--- src/gateway/models.py
+++ src/gateway/models.py
@@ -8,3 +8,8 @@ from common.models.mixins.timestamps import TimeStampMixin
 class Gateway(TimeStampMixin, models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     name = models.CharField(max_length=50, unique=True, blank=False, null=False)
+    mac_address = models.CharField(
+        max_length=17,
+        unique=True,
+        blank=False,
+    )
