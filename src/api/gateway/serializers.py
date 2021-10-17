807157962deb591d66e8aa91b295307d652e02fc
Jeff Labonte <grimsleepless@protonmail.com>
2021-10-17 11:43:49 -0400

    Update serializers to remove sensor and add only gateway

--- src/api/gateway/serializers.py
+++ src/api/gateway/serializers.py
@@ -1,33 +1,17 @@
 from rest_framework import serializers
 
 from gateway.models import Gateway
-from sensors.constants import TYPE_CHOICES
-from sensors.models import Sensors
-
-
-class SensorsWriteSerializer(serializers.ModelSerializer):
-    type = serializers.ChoiceField(
-        choices=TYPE_CHOICES,
-    )
-
-    def create(self, validated_data):
-        print(self.parent)
-        return super().create(validated_data)
-
-    class Meta:
-        model = Sensors
-        fields = ("type",)
 
 
 class GatewayWriteSerializer(serializers.ModelSerializer):
     name = serializers.CharField(
         max_length=50,
     )
-    sensors = SensorsWriteSerializer(many=True)
+    mac_address = serializers.CharField(max_length=17,)
 
     class Meta:
         model = Gateway
         fields = (
             "name",
-            "sensors",
+            "mac_address",
         )
