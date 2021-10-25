179d8b7f5543e8af7c130c0477664168aa395f7c
Jeff Labonte <grimsleepless@protonmail.com>
2021-10-25 00:12:51 -0400

    Update sensor/models.py
    Remove IpAddress from the models, I want to implement it later. Add TODO to add the model or part number of the sensor. Perhaps cross-referent with digi-key on user interface would be nice. Add related_name to make this relation available from the Gateway.

--- src/sensor/models.py
+++ src/sensor/models.py
@@ -6,39 +6,24 @@ from common.models.mixins.timestamps import TimeStampMixin
 from sensor.constants import TYPE_CHOICES
 
 
-class IpAddress(
-    TimeStampMixin,
-    models.Model,
-):
-    id = models.UUIDField(
-        primary_key=True,
-        default=uuid.uuid4,
-    )
-    gateway = models.ForeignKey(
-        to="gateway.Gateway",
-        on_delete=models.CASCADE,
-        related_name="gateway_sensors",
-    )
-    ip_address = models.CharField(
-        max_length=15,
-    )
-
-
 class Sensor(
     TimeStampMixin,
     models.Model,
 ):
+    # TODO Add model/part number of the sensor: cross-referent to digi-key/Amazon
     id = models.UUIDField(
         primary_key=True,
         default=uuid.uuid4,
+    )
+    name = models.CharField(
+        max_length=60,
+        blank=False,
+        null=False,
     )
     type = models.CharField(
         max_length=30,
         choices=TYPE_CHOICES,
     )
     gateway = models.ForeignKey(
-        to="gateway.Gateway",
-        default=None,
-        null=True,
-        on_delete=models.CASCADE,
+        to="gateway.Gateway", default=None, null=True, on_delete=models.CASCADE, related_name="gateway_sensors"
     )
