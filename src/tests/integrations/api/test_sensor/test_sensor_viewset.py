0eb662dd00303d696eb3fd10a14b6f297a9b6dd2
Jeff Labonte <grimsleepless@protonmail.com>
2021-10-30 01:51:16 -0400

    Create test to validate integration

--- src/tests/integrations/api/test_sensor/test_sensor_viewset.py
+++ src/tests/integrations/api/test_sensor/test_sensor_viewset.py
@@ -1,9 +1,40 @@
+from contextlib import contextmanager
+
 import pytest
+from django.urls.base import reverse
+from rest_framework.status import HTTP_200_OK
 from rest_framework.test import APIClient
+
+from sensor.models import Sensor
+
+
+@contextmanager
+def create_sensors():
+    sensors = [
+        Sensor.objects.create(
+            type="humidity",
+        ),
+        Sensor.objects.create(
+            type="temperature",
+        ),
+    ]
+    yield sensors
+    Sensor.objects.filter(
+        pk__in=[sensor.pk for sensor in sensors],
+    ).delete()
 
 
 @pytest.mark.django_db
 def test__sensor_viewset__list_should_return_multiple_sensors(
     auth_client: APIClient,
 ):
-    pass
+    with create_sensors() as sensors:
+        url = reverse("sensor-list")
+        response = auth_client.get(
+            path=url,
+        )
+
+        assert response.status_code == HTTP_200_OK
+
+        response_json = response.json()
+        assert len(response_json) == len(sensors)
