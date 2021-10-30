9e1251faa68fcc0d1058709e9b30ddbada5d4f87
Jeff Labonte <grimsleepless@protonmail.com>
2021-10-30 01:50:31 -0400

    Add new path to URLs

--- src/api/urls.py
+++ src/api/urls.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 
+
+api_urls = [
+    path("/", include("api.sensor.urls")),
+    path("/", include("api.gateway.urls")),
+]
+
+
 urlpatterns = [
     path("admin", admin.site.urls),
-    path("api/", include("api.gateway.urls")),
+    path("api", include(api_urls)),
     path("api/auth/", include("dj_rest_auth.urls")),
     path("api/auth/registration", include("dj_rest_auth.registration.urls")),
 ]
