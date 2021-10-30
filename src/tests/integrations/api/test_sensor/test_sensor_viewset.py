2a95c74f2804f118c1a373d569bffbc7d5f498f8
Jeff Labonte <grimsleepless@protonmail.com>
2021-10-30 01:11:44 -0400

    Fix fixture name

--- src/tests/integrations/api/test_sensor/test_sensor_viewset.py
+++ src/tests/integrations/api/test_sensor/test_sensor_viewset.py
@@ -4,6 +4,6 @@ from rest_framework.test import APIClient
 
 @pytest.mark.django_db
 def test__sensor_viewset__list_should_return_multiple_sensors(
-    auth_api: APIClient,
+    auth_client: APIClient,
 ):
     pass
