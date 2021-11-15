986edd72543ebb4f32017e920af8bbcc7f7c0ab6
Jeff Labonte <grimsleepless@protonmail.com>
2021-11-14 23:47:52 -0500

    Complete SensorTargetFactory

--- src/tests/factories/sensor_target_factory.py
+++ src/tests/factories/sensor_target_factory.py
@@ -1,6 +1,25 @@
 from factory import Factory
 from factory.faker import Faker
+from sensor_target.constants import SUPPORTED_TARGET_TYPE
+
+from sensor_target.models import SensorTarget
+from tests.factories.common import lazy_random_attribute
 
 
 class SensorTargetFactory(Factory):
-    id = Faker("mac_address")
+    id = Faker("uuid4")
+    type = lazy_random_attribute(
+        choices=list(SUPPORTED_TARGET_TYPE),
+    )
+    name = lazy_random_attribute(
+        choices=[
+            "Mushroom A",
+            "Cannabis Indica Strain Plant",
+            "Chives",
+            "Garlic 1",
+            "Garlic 3",
+        ],
+    )
+
+    class Meta:
+        model = SensorTarget
