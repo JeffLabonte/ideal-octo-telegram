852a8f634b9e9b0292ce12db110bb90f38b29484
Jeff Labonte <grimsleepless@protonmail.com>
2021-11-15 20:45:37 -0500

    Add test to create Sensor Target

--- src/tests/integrations/api/test_sensor_target/test_sensor_target_viewset.py
+++ src/tests/integrations/api/test_sensor_target/test_sensor_target_viewset.py
@@ -1,6 +1,7 @@
+import json
 from django.urls.base import reverse
 import pytest
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
 from rest_framework.test import APIClient
 
 from tests.factories.sensor_target_factory import SensorTargetFactory
@@ -26,3 +27,27 @@ def test__sensor_target_viewset__list_sensor_target(
     response_json = response.json()
     for sensor_target_kwargs in response_json:
         assert SensorTargetFactory.build(**sensor_target_kwargs) in sensor_targets
+
+
+@pytest.mark.django_db
+def test__sensor_target_viewset__create_sensor_target(
+    auth_client: APIClient,
+):
+    target_payload = {
+        "type": "plant",
+        "name": "Indica Plant 1",
+    }
+
+    url = reverse("target-list")
+    response = auth_client.post(
+        path=url,
+        data=json.dumps(target_payload),
+        content_type="application/json",
+    )
+
+    assert response.status_code == HTTP_201_CREATED
+
+    response_json = response.json()
+
+    assert response_json["name"] == "Indica Plant 1"
+    assert response_json["type"] == "plant"
