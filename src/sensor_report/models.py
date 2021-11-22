265161b406d8356e0745dc1b87ef0affdea99b5a
Jeff Labonte <grimsleepless@protonmail.com>
2021-11-21 22:49:39 -0500

    Update migrations

--- src/sensor_report/models.py
+++ src/sensor_report/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from sensor_report.constants import SUPPORTED_TYPES
 
 
-class SensorTarget(models.Model):
+class SensorReport(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     type = models.CharField(
         max_length=5,
