d08be57d3ad6ea1311da64f961582600a5ad7d07
Jeff Labonte <grimsleepless@protonmail.com>
2021-11-21 23:26:57 -0500

    Create SensorReporGetSerializer

---
+++ src/api/sensor_report/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from api.sensor.serializers import SensorGetSerializer
+from api.sensor_target.serializers import SensorTargetGetSerializer
+
+from sensor_report.models import SensorReport
+
+
+class SensorReportGetSerializer(serializers.ModelField):
+    id = serializers.URLField()
+    sensor = SensorGetSerializer()
+    target = SensorTargetGetSerializer()
+
+    class Meta:
+        model = SensorReport
+        fields = [
+            "id",
+            "type",
+            "value",
+            "sensor",
+            "target",
+        ]
