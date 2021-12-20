e7272823b27165361b46f04f21ae7059c0a92150
Jeff Labonte <grimsleepless@protonmail.com>
2021-12-19 23:04:42 -0500

    Create list and create route in SensorReportViewset

--- src/api/sensor_report/views.py
+++ src/api/sensor_report/views.py
@@ -1,5 +1,14 @@
+from rest_framework.mixins import CreateModelMixin, ListModelMixin
+from api.sensor_report.serializers import SensorReportGetSerializer, SensorReportWriteSerializer
 from common.views.common_viewset import CommonViewSet
 
 
-class SensorReportViewSet(CommonViewSet):
-    serializer_class = {}
+class SensorReportViewSet(
+    CreateModelMixin,
+    ListModelMixin,
+    CommonViewSet,
+):
+    serializer_class = {
+        "list": SensorReportGetSerializer,
+        "create": SensorReportWriteSerializer,
+    }
