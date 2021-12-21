c7d04717e79aee80aae11b1189888b0fedaad927
Jeff Labonte <grimsleepless@protonmail.com>
2021-12-20 22:41:59 -0500

    Change type for value_type and add constraint on value

--- src/sensor_report/models.py
+++ src/sensor_report/models.py
@@ -7,12 +7,14 @@ from sensor_report.constants import SUPPORTED_TYPES
 
 class SensorReport(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    type = models.CharField(
+    value_type = models.CharField(
         max_length=5,
         choices=SUPPORTED_TYPES,
     )
     value = models.CharField(
         max_length=20,
+        blank=False,
+        null=False,
     )
     sensor = models.ForeignKey(
         "sensor.Sensor",
