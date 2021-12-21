95787181117c71452d342ab5a89a6333aac9dec3
Jeff Labonte <grimsleepless@protonmail.com>
2021-12-20 22:57:20 -0500

    Create the SensorReportWriteSerializer

--- src/api/sensor_report/serializers.py
+++ src/api/sensor_report/serializers.py
@@ -1,8 +1,12 @@
+import uuid
 from rest_framework import serializers
 from api.sensor.serializers import SensorGetSerializer
 from api.sensor_target.serializers import SensorTargetGetSerializer
+from sensor_report.constants import SUPPORTED_TYPES
 
 from sensor_report.models import SensorReport
+from sensor_target.models import SensorTarget
+from sensor.models import Sensor
 
 
 class SensorReportGetSerializer(serializers.ModelField):
@@ -22,5 +26,33 @@ class SensorReportGetSerializer(serializers.ModelField):
 
 
 class SensorReportWriteSerializer(serializers.ModelField):
+    id = serializers.HiddenField(
+        default=uuid.uuid4(),
+        partial=False,
+    )
+    value_type = serializers.ChoiceField(
+        choices=SUPPORTED_TYPES,
+        required=True,
+    )
+    value = serializers.CharField(
+        max_length=20,
+        required=True,
+        allow_null=False,
+    )
+    sensor = serializers.PrimaryKeyRelatedField(
+        required=True,
+        queryset=Sensor.objects.all(),
+    )
+    target = serializers.PrimaryKeyRelatedField(
+        required=True,
+        queryset=SensorTarget.objects.all(),
+    )
+
     class Meta:
         model = SensorReport
+        fields = (
+            "value_type",
+            "value",
+            "sensor",
+            "target",
+        )
