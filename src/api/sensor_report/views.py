520122c4706d3e2420321af7f6918f360bbd7e38
Jeff Labonte <grimsleepless@protonmail.com>
2021-12-26 13:20:26 -0500

    Add queryset to SensorReportViewSet

--- src/api/sensor_report/views.py
+++ src/api/sensor_report/views.py
@@ -1,6 +1,7 @@
 from rest_framework.mixins import CreateModelMixin, ListModelMixin
 from api.sensor_report.serializers import SensorReportGetSerializer, SensorReportWriteSerializer
 from common.views.common_viewset import CommonViewSet
+from sensor_report.models import SensorReport
 
 
 class SensorReportViewSet(
@@ -8,6 +9,8 @@ class SensorReportViewSet(
     ListModelMixin,
     CommonViewSet,
 ):
+    queryset = SensorReport.objects.all()
+
     serializer_class = {
         "list": SensorReportGetSerializer,
         "create": SensorReportWriteSerializer,
