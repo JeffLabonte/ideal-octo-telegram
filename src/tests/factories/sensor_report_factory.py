f516a6343989a7f8106c5e721a6131a214b0fe4c
Jeff Labonte <grimsleepless@protonmail.com>
2021-12-26 13:24:57 -0500

    Fix SensorReportFactory to have all attributes

--- src/tests/factories/sensor_report_factory.py
+++ src/tests/factories/sensor_report_factory.py
@@ -1,8 +1,24 @@
+from random import randint
+
+import factory
 from factory.base import Factory
+from factory.faker import Faker
+from sensor_report.constants import SUPPORTED_TYPES
 
 from sensor_report.models import SensorReport
+from tests.factories.common import lazy_random_attribute
+from tests.factories.sensor_factory import SensorFactory
+from tests.factories.sensor_target_factory import SensorTargetFactory
 
 
 class SensorReportFactory(Factory):
+    id = Faker("uuid4")
+    value_type = lazy_random_attribute(
+        choices=list(SUPPORTED_TYPES),
+    )
+    value = str(randint(0, 99999))
+    sensor = factory.SubFactory(SensorFactory)
+    target = factory.SubFactory(SensorTargetFactory)
+
     class Meta:
         model = SensorReport
