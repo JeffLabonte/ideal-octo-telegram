3bb46d17a75442ab3a3605cb5547d8d7b2f523d4
Jeff Labonte <grimsleepless@protonmail.com>
2021-12-26 13:24:32 -0500

    Add subfactory in sensor_report_factory

--- src/tests/factories/sensor_factory.py
+++ src/tests/factories/sensor_factory.py
@@ -1,18 +1,17 @@
-from factory import Factory
-from factory.declarations import LazyAttribute
+import factory
 from factory.faker import Faker
-from sensor.constants import TYPE_CHOICES
 
+from sensor.constants import TYPE_CHOICES
 from sensor.models import Sensor
 from tests.factories.common import lazy_random_attribute
 from tests.factories.gateway_factory import GatewayFactory
 
 
-class SensorFactory(Factory):
+class SensorFactory(factory.Factory):
     id = Faker("uuid4")
     name = Faker("domain_name")
-    type = lazy_random_attribute(choices=TYPE_CHOICES)
-    gateway = LazyAttribute(lambda: GatewayFactory.create())
+    type = lazy_random_attribute(choices=list(TYPE_CHOICES))
+    gateway = factory.SubFactory(GatewayFactory)
 
     class Meta:
         model = Sensor
