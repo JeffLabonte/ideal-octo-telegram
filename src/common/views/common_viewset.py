d0fe33cea4dc920267d7825d50df10dcc6939d04
Jeff Labonte <grimsleepless@protonmail.com>
2021-12-26 13:23:00 -0500

    Update CommonViewSet

--- src/common/views/common_viewset.py
+++ src/common/views/common_viewset.py
@@ -1,9 +1,10 @@
+from rest_framework.generics import CreateAPIView
+from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
 
 
 class CommonViewSet(GenericViewSet):
     def get_serializer_class(self):
-        if not getattr(self, "serializer_class", {}):
-            raise NotImplementedError("You need to implement `serializer_class`")
-
-        return self.serializer_class.get(self.action)
+        if not (serializer_class := getattr(self, "serializer_class", {})):
+            raise NotImplementedError("You need to implement `serializer_class` in your view")
+        return serializer_class.get(self.action)
