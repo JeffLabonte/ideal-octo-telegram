c4ec4c17b4e67125fd3463cc9106dea44be4f6a5
Jeff Labonte <grimsleepless@protonmail.com>
2022-03-12 17:15:36 -0500

    Update databases config in dev

--- src/settings/dev.py
+++ src/settings/dev.py
@@ -1,1 +1,13 @@
 from settings.base import *
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "ideal-octo-telegram",
+        "USER": "dev_user",
+        "PASSWORD": "dev_user",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
