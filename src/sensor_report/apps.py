31b513f56b6fb7e6fa5240af447d2165770dd1ef
Jeff Labonte <grimsleepless@protonmail.com>
2022-04-18 01:25:56 -0400

    Add initial testing receiver code

--- src/sensor_report/apps.py
+++ src/sensor_report/apps.py
@@ -1,6 +1,19 @@
 from django.apps import AppConfig
 
+from mqtt_integration.mqtt_handler import MQTTConsumerHandler
+
+
+def callback(ch, method, properties, body):
+    print(locals())
+
 
 class SensorReportConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "sensor_report"
+
+    def ready(self):
+        consumer_mqtt = MQTTConsumerHandler()
+        consumer_mqtt.bind_topic(topic="#", callback=callback)
+        consumer_mqtt.start_process()
+
+        return super().ready()
