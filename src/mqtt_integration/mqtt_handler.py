fc7bc1a2eb019a499ce74ed865a7bf9d36c8704f
Jeff Labonte <grimsleepless@protonmail.com>
2022-05-07 02:20:44 -0400

    Remove hostname argument to use the settings

--- src/mqtt_integration/mqtt_handler.py
+++ src/mqtt_integration/mqtt_handler.py
@@ -1,14 +1,16 @@
 from multiprocessing import Process
 from typing import Callable
 
+from django.conf import settings
+
 import pika
 
 
 class BaseMQTT:
     EXCHANGE_NAME = "ideal_octo_telegram_topic"
 
-    def __init__(self, hostname="localhost") -> None:
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(hostname))
+    def __init__(self) -> None:
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(settings.MQTT_HOSTNAME))
         self.channel = self.connection.channel()
         self.create_queue()
 
@@ -22,9 +24,9 @@ class BaseMQTT:
 
 
 class MQTTConsumerHandler(BaseMQTT):
-    def __init__(self, hostname="localhost") -> None:
+    def __init__(self) -> None:
         self.process = None
-        super().__init__(hostname=hostname)
+        super().__init__()
 
     def bind_topic(self, topic: str, callback: Callable) -> None:
         self.channel.queue_bind(
